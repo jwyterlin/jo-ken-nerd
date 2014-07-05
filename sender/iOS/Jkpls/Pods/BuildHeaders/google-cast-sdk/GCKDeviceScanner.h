3a1973b9c17efec0ec3ccfac47aaea59fa1383f5
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2014-07-05 15:29:54 -0300

    Instalando cocoapods com lib do chromecast

---
+++ sender/iOS/Jkpls/Pods/BuildHeaders/google-cast-sdk/GCKDeviceScanner.h
@@ -0,0 +1,1 @@
+../../google-cast-sdk/GoogleCastFramework-2.2.1-Release/GoogleCast.framework/Versions/A/Headers/GCKDeviceScanner.h
---
+++ sender/iOS/Jkpls/Pods/google-cast-sdk/GoogleCastFramework-2.2.1-Release/GoogleCast.framework/Versions/A/Headers/GoogleCast.h
@@ -0,0 +1,22 @@
+// Cast SDK v2.2.1
+// Build 633, generated on 2014-05-21 10:42.
+// Copyright 2013-2014 Google Inc.
+
+
+#import <GoogleCast/GCKApplicationMetadata.h>
+#import <GoogleCast/GCKCastChannel.h>
+#import <GoogleCast/GCKDevice.h>
+#import <GoogleCast/GCKDeviceFilter.h>
+#import <GoogleCast/GCKDeviceManager.h>
+#import <GoogleCast/GCKDeviceScanner.h>
+#import <GoogleCast/GCKError.h>
+#import <GoogleCast/GCKFilterCriteria.h>
+#import <GoogleCast/GCKImage.h>
+#import <GoogleCast/GCKJSONUtils.h>
+#import <GoogleCast/GCKLogger.h>
+#import <GoogleCast/GCKMediaControlChannel.h>
+#import <GoogleCast/GCKMediaInformation.h>
+#import <GoogleCast/GCKMediaMetadata.h>
+#import <GoogleCast/GCKMediaStatus.h>
+#import <GoogleCast/GCKNSDictionary+TypedValueLookup.h>
+#import <GoogleCast/GCKSenderApplicationInfo.h>
