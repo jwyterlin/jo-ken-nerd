733980f337d619e427c0e4ebf4e25f7e9b369e93
Jhonathan Wyterlin <jhonathan_wyterlin@yahoo.com.br>
2015-04-24 01:26:21 -0300

    Created page-based views with choices to make in the game.

---
+++ sender/iOS/Jkpls/nnn WatchKit Extension/NotificationController.h
@@ -0,0 +1,14 @@
+//
+//  NotificationController.h
+//  nnn WatchKit Extension
+//
+//  Created by Jhonathan Wyterlin on 24/04/15.
+//  Copyright (c) 2015 Little Red Club. All rights reserved.
+//
+
+#import <WatchKit/WatchKit.h>
+#import <Foundation/Foundation.h>
+
+@interface NotificationController : WKUserNotificationInterfaceController
+
+@end
